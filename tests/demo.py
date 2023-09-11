cb838ce1cb7f8c6ecdb3bda1a92983c3490ee415
Byxs20 <97766819@qq.com>
2023-09-11 10:40:01 +0800

    update demo.py

--- tests/demo.py
+++ tests/demo.py
@@ -8,14 +8,14 @@ flowPath = os.path.join(baseDir, "flow.pcapng")
 display_filter = "(http.request and urlencoded-form) or (http.request and data-text-lines) or (http.request and mime_multipart) or (http.response.code == 200 and data-text-lines)"
 
 jsonPath = FlowAnalyzer.get_json_data(flowPath, display_filter=display_filter)
-for count, dic in enumerate(FlowAnalyzer(jsonPath).generate_http_dict_pairs(), start=1):
+for count, http in enumerate(FlowAnalyzer(jsonPath).generate_http_dict_pairs(), start=1):
     print(f"[+] 正在处理第{count}个HTTP流!")
     
-    request, response = dic.get("request"), dic["response"]
+    request, response = http.request, http.response
     if request:
-        request_num, header, file_data, time_epoch = request['frame_num'], request['header'], request['file_data'], request['time_epoch']
+        request_num, header, file_data, time_epoch = request.frame_num, request.header, request.file_data, request.time_epoch
         print("序号: {}请求包, 请求头: {}, 文件: {}, 时间: {}".format(request_num, header, file_data, time_epoch))
 
     if response:
-        response_num, header, file_data, time_epoch = response['frame_num'], response['header'], response['file_data'], response['time_epoch']
+        response_num, header, file_data, time_epoch = response.frame_num, response.header, response.file_data, response.time_epoch
         print("序号: {}请求包, 请求头: {}, 文件: {}, 时间: {}".format(response_num, header, file_data, time_epoch))
