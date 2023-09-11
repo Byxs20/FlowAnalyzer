f920e8b87ad2a2cc59bb21e8b67c91b6e1833cbd
Byxs20 <97766819@qq.com>
2023-09-11 10:40:27 +0800

    update version0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
 
 setup(
     name="FlowAnalyzer",
-    version="0.2.0",
+    version="0.2.1",
     description="FlowAnalyzer是一个流量分析器，用于解析和处理tshark导出的JSON数据文件",
     author="Byxs20",
     author_email="97766819@qq.com",
