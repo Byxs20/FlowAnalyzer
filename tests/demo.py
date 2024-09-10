89a72c48046d86190ddf821e6b418f39237db6c5
Byxs20 <97766819@qq.com>
2024-09-10 09:22:29 +0800

    修改: jsonPath 改为 josn_path

--- tests/demo.py
+++ tests/demo.py
@@ -7,8 +7,8 @@ baseDir = os.path.dirname(os.path.abspath(__file__))
 flowPath = os.path.join(baseDir, "flow.pcapng")
 display_filter = "(http.request and urlencoded-form) or (http.request and data-text-lines) or (http.request and mime_multipart) or (http.response.code == 200 and data-text-lines)"
 
-jsonPath = FlowAnalyzer.get_json_data(flowPath, display_filter=display_filter)
-for http_seq_num, http in enumerate(FlowAnalyzer(jsonPath).generate_http_dict_pairs(), start=1):
+json_path = FlowAnalyzer.get_json_data(flowPath, display_filter=display_filter)
+for http_seq_num, http in enumerate(FlowAnalyzer(json_path).generate_http_dict_pairs(), start=1):
     print(f"[+] 正在处理第{http_seq_num}个HTTP流!")
     
     request, response = http.request, http.response
