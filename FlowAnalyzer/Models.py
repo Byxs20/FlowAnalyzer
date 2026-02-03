8898b26f89e24f94f26a8aa39173d6b3c1d38a6b
Byxs20 <97766819@qq.com>
2026-02-03 16:52:36 +0800

    添加: 返回包状态码

--- FlowAnalyzer/Models.py
+++ FlowAnalyzer/Models.py
@@ -8,17 +8,18 @@ class Request:
     frame_num: int
     header: bytes
     file_data: bytes
-    full_uri: str
     time_epoch: float
+    full_uri: str
 
 
 @dataclass
 class Response:
-    __slots__ = ("frame_num", "header", "file_data", "time_epoch", "_request_in")
+    __slots__ = ("frame_num", "header", "file_data", "time_epoch", "status_code", "_request_in")
     frame_num: int
     header: bytes
     file_data: bytes
     time_epoch: float
+    status_code: int
     _request_in: Optional[int]
 
 
